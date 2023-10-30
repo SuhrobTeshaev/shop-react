e12927b46d1b68d71a67edde83de19268851835e
SuhrobTeshaev <125867321+SuhrobTeshaev@users.noreply.github.com>
2023-10-31 01:30:23 +0700

    update Product,Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import s from './../../styles/Home.module.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Poster from "./../Poster/Poster";
+import Banner from "./../Banner/Banner";
+import Categories from "./../Categories/Categories";
+import Products from "./../Products/Products";
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const {
+    products: { list, filtered },
+    categories,
+  } = useSelector((state) => state);
+  useEffect(
+    () => {
+      if ((!list, length)) return;
+      dispatch(filterByPrice(100));
+    },
+    dispatch,
+    list.length
+  );
   return (
     <>
-    постер
-    продукт
-    категориес
-    баннер
-    продуктс
+      <Poster />
+      <Products products={list} amount={5} title="Trending" />
+      <Categories products={categories.list} amount={5} title="Worth seeing" />
+      <Banner />
+      <Products products={filtered} amount={5} title="Less than 100$" />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
-import s from './../../styles/Product.module.css'
-const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+import React, { useEffect, useState } from "react";
+import s from "./../../styles/Product.module.css";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import ROUTES from './../../utils/Routes'
 
-export default Product
+const SIZES = [4, 4.5, 5];
+const Product = (item) => {
+  const { title, price, images, description } = item;
+  const dispatch = useDispatch();
+  const [currentImage, setCurrentImage] = useState();
+  const [currentSize, setCurrentSize] = useState();
+
+  useEffect(() => {
+    if (!images.length) return;
+    setCurrentImage(images[0]);
+  }, [images]);
+
+  const addToCart = () => {
+    dispatch(addToCart(item));
+  };
+  return (
+    <section className={s.product}>
+      <div className={s.images}>
+        <div
+          className={s.current}
+          style={{ backgroundImage: `url(${currentImage})` }}
+        />
+        <div>
+          {images.map((image, i) => (
+            <div
+              key={i}
+              className={s.image}
+              style={{ backgroundImage: `url(${image})` }}
+              onClick={() => setCurrentImage(image)}
+            />
+          ))}
+        </div>
+      </div>
+      <div className={s.info}>
+        <h1 className={s.title}>{title}</h1>
+        <div className={s.price}>{price}$</div>
+        <div className={s.color}>
+          <span>Color:</span>Green
+        </div>
+        <div className={s.sizes}>
+          <span>Sizes:</span>
+          <div className={s.list}>
+            {SIZES.map((size) => {
+              <div
+                onClick={() => setCurrentSize(size)}
+                className={`${s.sizes}
+               ${currentSize === size ? s.active : ""
+              }`}
+                key={size}
+              >
+                {size}
+              </div>;
+            })}
+          </div>
+        </div>
+        <p className={s.description}>{description}</p>
+        <div className={s.action}>
+          <button
+          onClick={addToCart}
+          className={s.add}
+          disabled={!currentSize}
+          >
+            Add to cart
+            </button>
+            <button className={s.favourite}>Add to favourites</button>
+        </div>
+        <div className={s.bottom}>
+          <div className={s.purchase}>33 people purchased</div>
+          <Link to={ROUTES.HOME}>Return to store</Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
